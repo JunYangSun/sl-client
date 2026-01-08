45647e6094e4efe032a649629092f34d50e1e54b
孙君洋 <yang@sunjunyangdeMacBook-Pro.local>
2026-01-08 21:34:23 +0800

    profile 使用不同的navbar

--- src/app/[locale]/templates/default/(profile)/layout.tsx
+++ src/app/[locale]/templates/default/(profile)/layout.tsx
@@ -1,19 +1,29 @@
+import { getLayoutData } from "@/lib/logic/common/layout";
 import BottomNavbar from "@/components/layout/BottomNavbar";
 import ProfileNavbar from "../components/layout/ProfileNavbar";
+import Navbar from "../components/layout/Navbar";
 
 /**
  * 默认模板 Profile 布局 - Server Component
- * 使用专用 Profile Navbar
+ * 移动端使用 ProfileNavbar，PC端使用 Navbar
  */
 export default async function DefaultProfileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const data = await getLayoutData();
 
   return (
     <>
-      <ProfileNavbar />
+      {/* 移动端：显示 ProfileNavbar */}
+      <div className="md:hidden">
+        <ProfileNavbar />
+      </div>
+      {/* PC端：显示 Navbar */}
+      <div className="hidden md:block">
+        <Navbar data={data} />
+      </div>
       <main className="bg-background pb-16 md:pb-0 md:min-h-screen">
         {children}
       </main>
